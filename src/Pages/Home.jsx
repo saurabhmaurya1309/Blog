21ca7925bccd8017347ad8e3b6eb2281521c7a6c
saurabhmaurya1309 <saurabhmaurya1309@gmail.com>
2023-10-02 01:14:11 +0530

    change

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Blogs from '../components/Blogs'
+import Header from '../components/Header'
+import Pagination from '../components/Pagination'
+
+const Home = () => {
+  return (
+    <div className="w-full h-full flex flex-col gap-y-1 justify-center items-center">
+      <Header/>
+      <div>
+        <Blogs/>
+        <Pagination/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Blogs.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import BlogDetails from "./BlogDetails";
+
+export default function Blogs() {
+  const { posts, loading } = useContext(AppContext);
+
+  return (
+    <div className='w-11/12 max-w-[670px] max-h-[100%] py-8 flex flex-col gap-y-7 mt-[50px] pb-[100px] mb-0  justify-center items-center'>
+      {loading ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">Loading</p>
+        </div>
+      ) : posts.length === 0 ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">No Blogs Found !</p>
+        </div>
+      ) : (
+        posts.map((post) => (
+          <BlogDetails key={post.id} post={post}/>
+        ))
+      )}
+    </div>
+  );
+}
